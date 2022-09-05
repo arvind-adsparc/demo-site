4581a6f27dfcaf98d8ebc9f52818678327ad7b4a
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-09-05 12:44:41 +0530

    h1 tag text change

---
+++ app.js
@@ -0,0 +1,27 @@
+// Filter Method
+// A fitler method can be used when you need to filter out certain elements from an array based on a conditon
+
+// For instance if we have a users data and we need to get rid of users below the age 25 from our list,
+//  we can make use of filter method
+const users = [
+  {
+    name: "Sam",
+    email: "sam@thecorp.com",
+    age: "24",
+  },
+  {
+    name: "Tom",
+    email: "sam@thecorp.com",
+    age: "26",
+  },
+  {
+    name: "Jim",
+    email: "sam@thecorp.com",
+    age: "29",
+  },
+  {
+    name: "Kim",
+    email: "sam@thecorp.com",
+    age: "25",
+  },
+];
